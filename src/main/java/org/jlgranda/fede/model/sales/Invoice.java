3e01e14fcf7dbe97828f63f4c71481360cba84b1
Fazt <kelly.narvaez1@gmail.com>
2021-03-10 13:15:53 -0500

    Issue Customers

--- src/main/java/org/jlgranda/fede/model/sales/Invoice.java
+++ src/main/java/org/jlgranda/fede/model/sales/Invoice.java
@@ -65,17 +65,13 @@ import org.slf4j.LoggerFactory;
     @NamedQuery(name = "Invoice.findByDocumentTypeAndOwner", query = "SELECT i FROM Invoice i WHERE i.documentType = ?1 AND i.owner = ?2 AND i.active=?3 AND i.emissionOn BETWEEN ?4 AND ?5 ORDER BY i.id DESC"),
     @NamedQuery(name = "Invoice.countByDocumentTypeAndOwner", query = "SELECT count(i) FROM Invoice i WHERE i.documentType = ?1 and i.owner = ?2 AND i.active=?3 AND i.emissionOn BETWEEN ?4 AND ?5"),
     @NamedQuery(name = "Invoice.countByDocumentTypeAndAuthor", query = "SELECT count(i) FROM Invoice i WHERE i.documentType = ?1 and i.author = ?2 AND i.active=?3"),
+    @NamedQuery(name = "Invoice.findTotalInvoiceSalesPaxBetween", query = "select sum(i.pax) from Invoice i WHERE i.author=?1 and i.documentType=?2 and i.status=?3 and i.emissionOn >= ?4 and i.emissionOn <= ?5"),
+    @NamedQuery(name = "Invoice.findTotalInvoiceSalesPaxEmissionBetween", query = "select i.pax, i.emissionOn from Invoice i WHERE i.author=?1 and i.documentType=?2 and i.status=?3 and i.emissionOn >= ?4 and i.emissionOn <= ?5"),
     @NamedQuery(name = "Invoice.findTotalInvoiceSalesDiscountBetween", query = "select sum(p.amount), sum(p.discount), sum(p.amount-p.discount) from Payment p LEFT JOIN p.invoice i WHERE i.author=?1 and i.documentType=?2 and i.status=?3 and i.emissionOn >= ?4 and i.emissionOn <= ?5"),
     @NamedQuery(name = "Invoice.findTotalInvoiceBussinesSalesDiscountBetween", query = "select i.code, i.boardNumber, i.emissionOn, p.amount, p.discount, i.id from Payment p LEFT JOIN p.invoice i WHERE i.author=?1 and i.documentType=?2 and i.status=?3 and i.emissionOn >= ?4 and i.emissionOn <= ?5 and p.discount> ?6"),
     @NamedQuery(name = "Invoice.countTotalInvoiceBetween", query = "select count(i) from Invoice i WHERE i.author=?1 and i.documentType=?2 and i.status=?3 and i.emissionOn >= ?4 and i.emissionOn <= ?5"),
     @NamedQuery(name = "Invoice.findTotalInvoiceSalesDiscountByOwnerBetween", query = "select sum(p.amount), sum(p.discount), sum(p.amount-p.discount) from Payment p LEFT JOIN p.invoice i WHERE i.author=?1 and i.owner=?2 and i.documentType=?3 and i.status=?4 and i.emissionOn >= ?5 and i.emissionOn <= ?6"),
 })
-//    SELECT 
-//bs.code, i.boardnumber, i.emissionon, pay.discount
-//	FROM public.payment as pay
-//	INNER JOIN public.invoice as i on i.invoiceid=pay.invoice_id
-//	INNER JOIN bussinesentity as bs on bs.id=i.invoiceid
-//	WHERE i.emissionon>='2021-03-01' AND i.emissionon<='2021-03-05' AND pay.discount>0;
 
 public class Invoice extends BussinesEntity {
     
@@ -125,7 +121,7 @@ public class Invoice extends BussinesEntity {
     protected String printAliasSummary;
     
     @Column(nullable = true, length = 1024)
-    protected String pax;
+    protected Long pax;
 
     public String getBoardNumber() {
         return boardNumber;
@@ -250,12 +246,12 @@ public class Invoice extends BussinesEntity {
     public void setPrintAliasSummary(String printAliasSummary) {
         this.printAliasSummary = printAliasSummary;
     }
-
-    public String getPax() {
+    @Transient
+    public Long getPax() {
         return pax;
     }
 
-    public void setPax(String pax) {
+    public void setPax(Long pax) {
         this.pax = pax;
     }
     
