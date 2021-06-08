753811cbced29d12ddabd9c2e65357a8893a4ef4
José Luis Granda <jlgranda81@gmail.com>
2021-06-08 13:20:51 -0500

    Soporte para tildes en mensajes e inicia pantalla de trámite con usuario logeado

--- src/main/java/net/tecnopro/helper/mail/VelocityHelper.java
+++ src/main/java/net/tecnopro/helper/mail/VelocityHelper.java
@@ -4,47 +4,48 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.Properties;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 
+public class VelocityHelper {
 
-public  class VelocityHelper {
+    public static String getRendererMessage(String message, Map values) throws Exception {
 
-	public static String getRendererMessage(String message, Map values) throws Exception{
-		
-		Properties p = new Properties();
-	    p.setProperty("file.resource.loader.path", "/tmp");
+        Properties p = new Properties();
+        p.setProperty("file.resource.loader.path", "/tmp");
 
-		/*  first, get and initialize an engine  */
+        /*  first, get and initialize an engine  */
         VelocityEngine ve = new VelocityEngine();
         ve.init(p);
         /*  next, get the Template  */
-        String templateFileName = createTemporaryFileTemplate(message);
+        String templateFileName = createTemporaryFileTemplate(StringEscapeUtils.escapeHtml4(message));
         Template t = ve.getTemplate(templateFileName);
         /*  create a context and add data */
         VelocityContext context = new VelocityContext(values);
         /* now render the template into a StringWriter */
         StringWriter writer = new StringWriter();
-        t.merge( context, writer );
-      
+        t.merge(context, writer);
+
         String rendererMessage = writer.toString();
         /* show the World */
-        System.out.println("VelocityHelper: Se renderizó el mensaje: " + rendererMessage );
+        //System.out.println("VelocityHelper: Se renderizó el mensaje: " + rendererMessage);
         //destroyTemporaryFileTemplate(templateFileName);
         return rendererMessage;
-		
-	}
-	
-	public static String getRendererMessage(String message, Map values, String templateFileName) throws Exception{
-		
-		Properties p = new Properties();
-	    p.setProperty("file.resource.loader.path", "/tmp");
 
-		/*  first, get and initialize an engine  */
+    }
+
+    public static String getRendererMessage(String message, Map values, String templateFileName) throws Exception {
+
+        Properties p = new Properties();
+        p.setProperty("file.resource.loader.path", "/tmp");
+
+        /*  first, get and initialize an engine  */
         VelocityEngine ve = new VelocityEngine();
         ve.init(p);
         /*  next, get the Template  */
@@ -53,36 +54,36 @@ public  class VelocityHelper {
         VelocityContext context = new VelocityContext(values);
         /* now render the template into a StringWriter */
         StringWriter writer = new StringWriter();
-        t.merge( context, writer );
-      
+        t.merge(context, writer);
+
         String rendererMessage = writer.toString();
         /* show the World */
         //System.out.println("VelocityHelper: Se renderizó el mensaje: " + rendererMessage );
         return rendererMessage;
-		
-	}
-	
-	public static String createTemporaryFileTemplate(String message) throws IOException{
-		File f=new File("/tmp");
-		File tmp = File.createTempFile("email_", ".vm",f);
-		if (tmp.canWrite()){
-			FileOutputStream stream = new FileOutputStream(tmp);
-			stream.write(message.getBytes());
-			stream.close();
-			System.out.println("VelocityHelper: se creo el archivo plantilla temporal: " + tmp.getAbsolutePath());
-		} else {
-			return "failure";
-		}
-		return tmp.getName();
-	}
-	
-	public static boolean destroyTemporaryFileTemplate(String filename) throws IOException{
-		
-		File tmp = new File( System.getProperty("java.io.tmpdir") + File.separator + filename);
-		if (tmp.canWrite()){
-			return tmp.delete();
-		} 
-		return false;
-	}
-	
+
+    }
+
+    public static String createTemporaryFileTemplate(String message) throws IOException {
+        File f = new File("/tmp");
+        File tmp = File.createTempFile("email_", ".vm", f);
+        if (tmp.canWrite()) {
+            FileOutputStream stream = new FileOutputStream(tmp);
+            stream.write(StringEscapeUtils.escapeHtml4(message).getBytes(Charset.defaultCharset()));
+            stream.close();
+            //System.out.println("VelocityHelper: se creo el archivo plantilla temporal: " + tmp.getAbsolutePath());
+        } else {
+            return "failure";
+        }
+        return tmp.getName();
+    }
+
+    public static boolean destroyTemporaryFileTemplate(String filename) throws IOException {
+
+        File tmp = new File(System.getProperty("java.io.tmpdir") + File.separator + filename);
+        if (tmp.canWrite()) {
+            return tmp.delete();
+        }
+        return false;
+    }
+
 }
