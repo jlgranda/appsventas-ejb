09ef60bfb6481c3a45070d813786133bd728f514
kellypaulinc <kelly.narvaez1@gmail.com>
2022-04-07 13:42:58 -0500

    Cierre de caja

--- src/main/java/com/jlgranda/fede/ejb/CashBoxGeneralService.java
+++ src/main/java/com/jlgranda/fede/ejb/CashBoxGeneralService.java
@@ -17,7 +17,9 @@
  */
 package com.jlgranda.fede.ejb;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -28,27 +30,29 @@ import javax.persistence.criteria.Root;
 import org.jlgranda.fede.model.accounting.CashBoxGeneral;
 import org.jlgranda.fede.model.accounting.CashBoxGeneral_;
 import org.jpapi.controller.BussinesEntityHome;
+import org.jpapi.model.Organization;
 import org.jpapi.model.StatusType;
 import org.jpapi.util.Dates;
+import org.jpapi.util.QuerySortOrder;
 
 /**
  *
  * @author author
  */
 @Stateless
-public class CashBoxGeneralService extends BussinesEntityHome<CashBoxGeneral>{
-    
+public class CashBoxGeneralService extends BussinesEntityHome<CashBoxGeneral> {
+
     @PersistenceContext
     EntityManager em;
-    
+
     @PostConstruct
-    private void init(){
+    private void init() {
         setEntityManager(em);
         setEntityClass(CashBoxGeneral.class);
     }
-    
+
     @Override
-    public CashBoxGeneral createInstance(){
+    public CashBoxGeneral createInstance() {
         CashBoxGeneral _instance = new CashBoxGeneral();
         _instance.setCreatedOn(Dates.now());
         _instance.setLastUpdate(Dates.now());
@@ -58,19 +62,35 @@ public class CashBoxGeneralService extends BussinesEntityHome<CashBoxGeneral>{
 //        _instance.setAuthor(null); //Establecer al usuario actual
         return _instance;
     }
-    
+
     //soporte para Lazy Data Model
     public long count() {
         return super.count(CashBoxGeneral.class);
     }
-    
-    public List<CashBoxGeneral> find(int maxresults, int firstresult){
-        
+
+    public List<CashBoxGeneral> find(int maxresults, int firstresult) {
+
         CriteriaBuilder builder = getCriteriaBuilder();
         CriteriaQuery<CashBoxGeneral> query = builder.createQuery(CashBoxGeneral.class);
-        
+
         Root<CashBoxGeneral> from = query.from(CashBoxGeneral.class);
         query.select(from).orderBy(builder.desc(from.get(CashBoxGeneral_.name)));
         return getResultList(query, maxresults, firstresult);
     }
+
+    public List<CashBoxGeneral> findByOrganization(Organization organization) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("organization", organization);
+        return this.find(-1, -1, "createdOn", QuerySortOrder.DESC, params).getResult();
+    }
+
+    public Long findIdByLastCashBoxGeneral(Organization organization) {
+        List<CashBoxGeneral> listCashBoxGeneral = this.findByOrganization(organization);
+        if (!listCashBoxGeneral.isEmpty()) {
+            return listCashBoxGeneral.get(0).getId();
+        } else {
+            return null;
+        }
+    }
+
 }
